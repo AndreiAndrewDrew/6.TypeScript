b67991badf8ad32e1b056bb1a776206d57633821
AndreiAndrewDrew <andreiandrewg@gmail.com>
2024-09-24 10:05:28 +0300

    03 Основные типы - 012 Объекты

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ const getFullNameArrow = (firstName, lastName) => {
     return `${firstName} ${lastName}`;
 };
 console.log(getFullName('Andrew', 'Boll'));
+const user = {
+    firstname: 'Timo',
+    surname: 'Boll',
+    city: 'Berlin',
+    age: 41,
+    skills: {
+        dev: true,
+        devops: true
+    }
+};
+function getNameUser(userEntity) {
+    userEntity.age = 45;
+    return `${userEntity.firstname} ${userEntity.surname} traieste in ${userEntity.city}, are ${userEntity.age} de ani.`;
+}
+console.log(getNameUser(user));
--- app.ts
+++ app.ts
@@ -8,3 +8,21 @@ const getFullNameArrow = (firstName: string, lastName: string): string => {
 };
 
 console.log(getFullName('Andrew', 'Boll'));
+
+const user = {
+  firstname: 'Timo',
+  surname: 'Boll',
+  city: 'Berlin',
+  age: 41,
+  skills: {
+    dev:true,
+    devops:true
+  }
+};
+
+function getNameUser(userEntity:{firstname:string, surname:string, city:string, age:number}):string{
+  userEntity.age=45;
+  return `${userEntity.firstname} ${userEntity.surname} traieste in ${userEntity.city}, are ${userEntity.age} de ani.`
+}
+
+console.log(getNameUser(user))
