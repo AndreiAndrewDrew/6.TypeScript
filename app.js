516dbefb84aa10aee598e8a766c37cac9ede70fc
AndreiAndrewDrew <andreiandrewg@gmail.com>
2024-09-25 14:30:23 +0300

    03 Основные типы - 015 Tuples

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 "use strict";
-const skills = ['Dev', 'DevOps', 'Testing'];
-for (const skill of skills) {
-    console.log(skill.toUpperCase());
-}
-const res = skills
-    .filter((s) => s !== "DevOps")
-    .map((s) => s + "!")
-    .reduce((a, b) => a + b);
-console.log(res);
+const skill = [1, "Dev"];
+const id = skill[0];
+const skillName = skill[1];
+skill.push('sdkjasdbfkj');
+// const sdadf = skill[2] //nu ne putem adressa la al 3 element,ca e tipizat fix 2 elemenet [number, string]
+skill.pop();
+const [id2, skillName2] = skill; //facem destructurizatioa lui tuple
+//informatsie adugatoare cu spread operator
+const arr = [1, 'sfdsd', true, true, false];
--- app.ts
+++ app.ts
@@ -1,11 +1,12 @@
-const skills: string[] = ['Dev','DevOps','Testing']
-for (const skill of skills){
-  console.log(skill.toUpperCase())
-}
+const skill: [number, string] = [1, "Dev"];
+const id = skill[0];
+const skillName = skill[1];
 
-const res = skills
-  .filter((s:string) => s !== "DevOps")
-  .map((s) => s + "!")
-  .reduce((a, b) => a + b);
+skill.push('sdkjasdbfkj')
+// const sdadf = skill[2] //nu ne putem adressa la al 3 element,ca e tipizat fix 2 elemenet [number, string]
+skill.pop()
 
-console.log(res)
+const [id2, skillName2] = skill //facem destructurizatioa lui tuple
+
+//informatsie adugatoare cu spread operator
+const arr: [number, string, ...boolean[]] = [1, 'sfdsd', true, true, false]
