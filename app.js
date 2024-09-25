e9380271be74b61a30986bc1ecedfd2da571b004
AndreiAndrewDrew <andreiandrewg@gmail.com>
2024-09-25 15:31:07 +0300

    03 Основные типы - 017 Enums

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
 "use strict";
-const skill = [1, "Dev"];
-const skills = ['Dev', 'DevOps'];
-// skills.push('bkb') //in readonly nu putem aduga element nou
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 1] = "SUCCESS";
+    StatusCode[StatusCode["IN_PROCESS"] = 2] = "IN_PROCESS";
+    StatusCode[StatusCode["FAILED"] = 3] = "FAILED";
+})(StatusCode || (StatusCode = {}));
+const res = {
+    message: "Tranzactie reusita",
+    statusCode: StatusCode.SUCCESS,
+};
+// 1 - tranzactie reusita
+// 2 - tranzactie in process
+// 3 - tranzactie respinsa
+if (res.statusCode === StatusCode.SUCCESS) {
+}
+//exemplu 2
+var StatusCode2;
+(function (StatusCode2) {
+    StatusCode2[StatusCode2["SUCCESS"] = 1] = "SUCCESS";
+    StatusCode2["IN_PROCESS"] = "p";
+    StatusCode2["FAILED"] = "f";
+})(StatusCode2 || (StatusCode2 = {}));
+const res2 = {
+    message: "Tranzactie reusita",
+    statusCode: StatusCode2.SUCCESS,
+};
+// s - tranzactie reusita
+// p - tranzactie in process
+// f - tranzactie respinsa
+function action(status) { }
+action(StatusCode2.SUCCESS);
+action(1);
+// action('p') //ne da euroare
+function compute() {
+    return 3;
+}
+var Roles;
+(function (Roles) {
+    Roles[Roles["ADMIN"] = 1] = "ADMIN";
+    Roles[Roles["USER"] = compute()] = "USER";
+})(Roles || (Roles = {}));
+const res3 = 1 /* Roles3.ADMIN */;
--- app.ts
+++ app.ts
@@ -1,5 +1,57 @@
-const skill: readonly[number, string] = [1, "Dev"];
+enum StatusCode {
+  SUCCESS = 1,
+  IN_PROCESS,
+  FAILED,
+}
 
-const skills : readonly string[] = ['Dev','DevOps']
+const res = {
+  message: "Tranzactie reusita",
+  statusCode: StatusCode.SUCCESS,
+};
 
-// skills.push('bkb') //in readonly nu putem aduga element nou
+// 1 - tranzactie reusita
+// 2 - tranzactie in process
+// 3 - tranzactie respinsa
+
+if (res.statusCode === StatusCode.SUCCESS) {
+}
+
+//exemplu 2
+
+enum StatusCode2 {
+  SUCCESS = 1,
+  IN_PROCESS = "p",
+  FAILED = "f",
+}
+
+const res2 = {
+  message: "Tranzactie reusita",
+  statusCode: StatusCode2.SUCCESS,
+};
+
+// s - tranzactie reusita
+// p - tranzactie in process
+// f - tranzactie respinsa
+
+function action(status: StatusCode2) {}
+
+action(StatusCode2.SUCCESS);
+action(1);
+// action('p') //ne da euroare
+
+function compute() {
+  return 3;
+}
+
+enum Roles {
+  ADMIN = 1,
+  USER = compute(),
+}
+
+//Exemplu 3
+const enum Roles3{
+  ADMIN = 1,
+  USER = 2
+}
+
+const res3 = Roles3.ADMIN
