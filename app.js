522af2bf5f232589cb6d9169661bb86b84ce4498
AndreiAndrewDrew <andreiandrewg@gmail.com>
2024-09-25 14:55:23 +0300

    03 Основные типы - 016 Readonly

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 "use strict";
 const skill = [1, "Dev"];
-const id = skill[0];
-const skillName = skill[1];
-skill.push('sdkjasdbfkj');
-// const sdadf = skill[2] //nu ne putem adressa la al 3 element,ca e tipizat fix 2 elemenet [number, string]
-skill.pop();
-const [id2, skillName2] = skill; //facem destructurizatioa lui tuple
-//informatsie adugatoare cu spread operator
-const arr = [1, 'sfdsd', true, true, false];
+const skills = ['Dev', 'DevOps'];
+// skills.push('bkb') //in readonly nu putem aduga element nou
--- app.ts
+++ app.ts
@@ -1,12 +1,5 @@
-const skill: [number, string] = [1, "Dev"];
-const id = skill[0];
-const skillName = skill[1];
+const skill: readonly[number, string] = [1, "Dev"];
 
-skill.push('sdkjasdbfkj')
-// const sdadf = skill[2] //nu ne putem adressa la al 3 element,ca e tipizat fix 2 elemenet [number, string]
-skill.pop()
+const skills : readonly string[] = ['Dev','DevOps']
 
-const [id2, skillName2] = skill //facem destructurizatioa lui tuple
-
-//informatsie adugatoare cu spread operator
-const arr: [number, string, ...boolean[]] = [1, 'sfdsd', true, true, false]
+// skills.push('bkb') //in readonly nu putem aduga element nou
