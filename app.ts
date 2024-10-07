27def71358a85243f5f298a913b5de8606ff70bd
AndreiAndrewDrew <andreiandrewg@gmail.com>
2024-10-07 11:05:12 +0300

    04 Продвинутые типы - 030 Null

--- app.ts
+++ app.ts
@@ -1,48 +1,27 @@
-//Exemplu 1 de folosire a tipului 'never'
-function genearteError(mesaj: string) : never{
-  throw new Error(mesaj)
-} 
+const n: null = null;
 
-//Ex.2
-function dumpError(): never{
-  // return ''; //ne da eroare
-  while(true){
+const n1: any = null;
 
+const n2: number = null;
+const n3: string = null;
+const n4: boolean = null;
+const n5: undefined = null;
+
+interface User {
+  name: string;
+}
+
+function getUser() {
+  if (Math.random() > 0.5) {
+    return null;
+  } else {
+    return {
+      name: "Vasea",
+    } as User;
   }
 }
 
-//Ex3
-function recursie(): never{
-  return recursie()
-}
-
-//Ex.4
-type paymentAction ='refund' | 'checkout' | 'reject'
-
-function processAction(action: paymentAction){
-   switch(action){
-    case 'refund':
-      //..ceva executam
-      break;
-    case 'checkout':
-      //..
-      break;
-    case 'reject':
-      //..
-      break;
-    default:
-      const _: never = action
-      throw new Error('Nu exista asa actsiune.')
-   }
-}
-
-//Ex.5
-function isString(x: string | number): boolean{
-  if (typeof x === 'string'){
-    return true;
-  }else if(typeof x === 'number') {
-    return false;
-  }
-  genearteError('sfasfjs') //functia din Ex.1 de mai sus
-
+const user = getUser();
+if (user) {
+  const numeUtilizator = user.name;
 }
