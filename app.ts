10e005184ab4354c294d454066bffd6dfcd73dc1
AndreiAndrewDrew <andreiandrewg@gmail.com>
2024-10-07 15:22:29 +0300

    04 Продвинутые типы - 032 Type Guard.mp4

--- app.ts
+++ app.ts
@@ -1,58 +1,46 @@
-let a = 5;
-let b:string = a.toString();
-
-//cu asa converatare sa fim atentsi
-let e:string = new String(a).valueOf();
-//Avem tip 'string', si avem 'String' cu litera mare car este constructor de obiecte
-let f: boolean = new Boolean(a).valueOf();
-
-let c = 'dasfsf';
-let d: number = parseInt(c);
-
-interface User{
+interface User {
   name: string;
-  email:string;
+  email: string;
   login: string;
 }
 
-//Metoda 1 de creeeare a 'user' de tip 'User'
-const user: User ={
-  name: 'vasile',
-  email: 'vasili@gmail.com',
-  login:'vasilica'
-}
+const user: User = {
+  name: "vasile",
+  email: "vasili@gmail.com",
+  login: "vasilica",
+};
 
-//Metoda 2
-const user2 = {
-  name: 'vasile2',
-  email: 'vasili2@gmail.com',
-  login:'vasilica2'
-} as User
-
-//Metoda 3 (Nu se recomanda de folosit)
-const user3 = <User> {
-  name: 'vasile3',
-  email: 'vasili3@gmail.com',
-  login:'vasilica3'
-} 
-
-interface Admin{
+interface Admin {
   name: string;
   role: number;
 }
 
-//Convertam 'user' nostru in 'admin'
-//Metoda 1 (Minusul acestei metode ii salvam la admin cimurile de la user)
-const admin: Admin = {
-  ...user,
-  role:1
+function logId(id: string | number) {
+  if (isString(id)) { //se foloseste functia 'isString' TypeGuard
+    console.log(id);//id este string
+  } else {
+    console.log(id);//id este number 
+  }
 }
 
-//Metoda 2 Care este Corecta in cazul dat
-//functia 'userToAdmin' cere converteaza numai cimpurile de care avem nevoie
-function userToAdmin(user: User):Admin{
-  return{
-    name: user.login,
-    role:1
+//creem functei simpla de Type Guard
+function isString(x: string | number): x is string{
+  return typeof x === "string";
+}
+
+//Functia typeGuard 'isAdmin'
+function isAdmin(user: User | Admin):user is Admin{
+  return 'role' in user;
+}
+
+function isAdminAlternative(user: User | Admin):user is Admin{
+  return (user as Admin).role !== undefined;
+}
+
+function setRoleZero(user: User | Admin){
+  if (isAdmin(user)){
+    user.role = 0
+  } else{
+    throw new Error('utilizaturl nu este Admin')
   }
 }
