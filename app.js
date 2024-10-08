54a1f27308a2fb8bb7d1698e451957d36237f331
AndreiAndrewDrew <andreiandrewg@gmail.com>
2024-10-08 11:14:03 +0300

    05 Классы - 036 Конструктор

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 "use strict";
 class User {
-    constructor(name) {
-        this.name = name;
+    //constructor de implementare
+    constructor(ageOrName, age) {
+        if (typeof ageOrName === 'string') {
+            this.name = ageOrName;
+        }
+        else if (typeof ageOrName === 'number') {
+            this.age = ageOrName;
+        }
+        if (typeof age === 'number') {
+            this.age = age;
+        }
     }
 }
-const utilizator = new User('Vasea');
-console.log(utilizator);
-utilizator.name = 'Petrica';
-console.log(utilizator);
-class Admin {
-}
-const administrator = new Admin();
-administrator.role = 1;
-console.log(administrator);
-console.log(administrator.role);
+const user1 = new User();
+console.log(user1);
+const user2 = new User('Vasea');
+console.log(user2);
+const user3 = new User(33);
+console.log(user3);
+const user4 = new User('Andrew', 38);
+console.log(user4);
--- app.ts
+++ app.ts
@@ -1,26 +1,29 @@
 class User{
   name: string;
+  age: number;
 
-  constructor(name:string){
-    this.name = name;
+  constructor();
+  constructor(name:string)
+  constructor(age: number);
+  constructor(name:string, age:number);
+  //constructor de implementare
+  constructor(ageOrName?:string | number, age?:number){
+    if (typeof ageOrName === 'string'){
+      this.name = ageOrName;
+    }else if(typeof ageOrName === 'number'){
+      this.age = ageOrName
+    }
+    if (typeof age === 'number'){
+      this.age =age
+    }
   }
 }
 
-const utilizator = new User('Vasea')
-console.log(utilizator)
-utilizator.name = 'Petrica'
-console.log(utilizator)
-
-class Admin{//declaram fara constructor,
-  //pentru asta este ncesar in fisierul 'tsconfig.json'
-  //Gasim cimpul "strictPropertyInitialization" po default el este 'true',
-  //Il descomentam si ii atribuim valoare 'false'. 
-  //Rezulta ca de constructor nu este deja necesar!!!
-  role: number;
-}
-
-const administrator = new Admin()
-administrator.role =1
-console.log(administrator);
-console.log(administrator.role)
-
+const user1 = new User()
+console.log(user1)
+const user2 = new User('Vasea')
+console.log(user2)
+const user3 = new User(33)
+console.log(user3)
+const user4 = new User('Andrew',38)
+console.log(user4)
