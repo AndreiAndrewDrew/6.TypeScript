9fcf19a3b363eddbafd627e3e3e7709b3c7fa266
AndreiAndrewDrew <andreiandrewg@gmail.com>
2024-10-23 09:31:57 +0300

    06 Компилятор TypeScript - 055 Модули

--- test/test3.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var PaymentStatus;
-(function (PaymentStatus) {
-    PaymentStatus["Succes"] = "succes";
-    PaymentStatus["Failed"] = "failed";
-})(PaymentStatus || (PaymentStatus = {}));
