0b159fe0a9cbddebacaa2e592a8efabc70b3a4aa
AndreiAndrewDrew <andreiandrewg@gmail.com>
2024-10-23 14:50:38 +0300

    07 Generics - 059 Пример встроенных generic

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,12 @@
-class User {
-  // name: string;
+const num: Array<number> = [1, 2, 3]; //<number> identificam ca este masiv de numere
 
-  // constructor(name:string){
-  //    this.name = name;
-  // }
-
-  //analogul mai scurt la cele 3 rinduri de mai sus
-  role?: 'admin'|'user'
-
-  constructor(public name:string){}
-
-  login(): void{}
+async function test() {
+  const p = new Promise<number>((resolve, reject) => {
+    resolve(1);
+  });
 }
 
-function createUser(user: User){
-  //logica
-  const defaultUser = new User('default');
-  defaultUser.role = undefined
-
-  switch (user.role){
-    case 'admin':
-      // return;
-      const a = 8;
-    case 'user':
-      return true;
-      const c=1;
-  }
-}
-
-interface IChecks {
-  [check: string ]: boolean;
-}
-
-const d: IChecks ={};
-const e = d['drive'];
-
-d.drive
-
-class VipUser extends User{
-  override login(): void {
-    
-  }
-}
+const check: Record<string, boolean> ={
+  drive: true,
+  kpp: false,
+};
