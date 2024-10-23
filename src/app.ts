0c5eb29f5876184ef759fd97a7f1fae3b44acb4f
AndreiAndrewDrew <andreiandrewg@gmail.com>
2024-10-23 10:33:52 +0300

    06 Компилятор TypeScript - 057 Проверки кода

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,44 @@
-function test(a: number): number | undefined {
-  if (a > 0) {
-    return a;
+class User {
+  // name: string;
+
+  // constructor(name:string){
+  //    this.name = name;
+  // }
+
+  //analogul mai scurt la cele 3 rinduri de mai sus
+  role?: 'admin'|'user'
+
+  constructor(public name:string){}
+
+  login(): void{}
+}
+
+function createUser(user: User){
+  //logica
+  const defaultUser = new User('default');
+  defaultUser.role = undefined
+
+  switch (user.role){
+    case 'admin':
+      // return;
+      const a = 8;
+    case 'user':
+      return true;
+      const c=1;
   }
 }
 
-//Exemplu 2
-function test2(a: number) {
-    return a;
+interface IChecks {
+  [check: string ]: boolean;
 }
 
-type StrOrNumFunct = (a: number | string) => number;
+const d: IChecks ={};
+const e = d['drive'];
 
-// let f:StrOrNumFunct = test2;
+d.drive
 
+class VipUser extends User{
+  override login(): void {
+    
+  }
+}
