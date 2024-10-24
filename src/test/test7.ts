3d443745346597cdaa4cb34900c1eaa13e6a706f
AndreiAndrewDrew <andreiandrewg@gmail.com>
2024-10-24 10:36:56 +0300

    07 Generics - 061 Упражнение - Функция преобразования в строку

---
+++ src/test/test7.ts
@@ -0,0 +1,35 @@
+/*
+Trebuie de scris functia 'toString' care primeste orice tip de date,
+si intoarce string din aceste date. Dar daca nu reuseste intoarce 'undefined'
+ */
+
+function toString<T>(data: T): string | undefined {
+  if (Array.isArray(data)) {
+    return data.toString();
+  }
+  switch (typeof data) {
+    case "string":
+      return data;
+    case "number":
+    case "symbol":
+    case "bigint":
+    case "boolean":
+    case "function":
+      return data.toString();
+    case "object":
+      return JSON.stringify(data);
+    default:
+      return undefined;
+  }
+}
+
+console.log(3);
+console.log(true);
+console.log('Andrew');
+console.log(['a','b','c']);
+console.log({a:1, b:2});
+console.log({c:10});
+
+
+
+
